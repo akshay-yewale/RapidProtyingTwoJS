f9ec61dfefd2be7fcb7b3e400d6958037e9b79ec
John Olav Haraldstad <john.haraldstad@hotmail.com>
2016-09-17 14:35:38 -0600

    sad
    Lots a changes to physics

--- js/main.js
+++ js/main.js
@@ -19,9 +19,9 @@ var livesLeft;
 GameState.prototype.create = function() {
 //				game.add.sprite(0,0,'background');
 				
-				this.GRAVITY = 30;
+				this.GRAVITY = 300;
 			  	game.stage.backgroundColor = "#4488AA";
-				
+				game.physics.startSystem(Phaser.Physics.ARCADE);
 
 				//adding player object to screen
 				this.player = this.game.add.sprite(75,75,'playerObject');
@@ -39,8 +39,8 @@ GameState.prototype.create = function() {
  				game.physics.arcade.gravity.y = this.GRAVITY;
 
  				this.player.body.allowGravity=false;
- 				//this.player.body.immovable=true;
- 				this.player.collideWorldBounds = true;
+ 				this.player.body.collideWorldBounds = true;
+ 				this.player.body.immovable = true;
 
 				
 				// adding ground blocks
@@ -60,19 +60,19 @@ GameState.prototype.create = function() {
 				game.physics.enable(this.person,Phaser.Physics.ARCADE);
 				this.person.x= 200;
 				this.person.y = 20;
-				this.person.body.immovable = true;
+
      			this.person.body.allowGravity=true;
-		      	this.person.collideWorldBounds = true;
-      			//this.person.body.checkCollision.down = true;
+		      	this.person.body.collideWorldBounds = true;
+                this.person.body.velocity.setTo(90, 240);
       			//this.person.body.checkCollision.up = true;
-				this.person.body.bounce.setTo(1,1);
+				this.person.body.bounce.setTo(1.01);
 
 
 				//this.game.time.advancedTiming = true;
 				// adding text to screen
 				livesLeft = 10;
 				//not working 
-				livesText= game.add.text(500,500,'LIVES LEFT : '+livesLeft, { font: "37px Arial", fill: "#4488AA" });
+
 
 };
 
@@ -80,28 +80,44 @@ GameState.prototype.create = function() {
 function updateUI()
 {
 
-	//not working
-		livesText= game.add.text(10,10,'LIVES LEFT : '+livesLeft, { font: "20px Arial", fill: "#4488AA" });	
+	//Change color
+		livesText= game.add.text(10,0,'LIVES LEFT : '+livesLeft, { font: "37px Arial", fill: "#000000" });	
 		this.livesText.setText("Fuel: " + livesLeft);
 
 };
 
+function dotproduct(a, b)
+{
+            return (a.x * b.x) + (a.y * b.y);
+}
+
  GameState.prototype.update = function() {
 
-		game.physics.arcade.collide(this.person,this.player);
+		if (game.physics.arcade.collide(this.person,this.player))
+		{
+			var norma = new Phaser.Point(); 
+			norma = (this.person.body.position.x - this.player.body.position.x, this.person.body.position.y - this.player.body.position.y);
+			norma.normalize();
+		}
+		if (game.physics.arcade.collide(this.person,this.ground))
+		{
+				this.person.body.velocity.setTo(0, 0);
+				this.person.game.physics.enable(false);
+		}
+
+
+		this.player.body.velocity.x = 0;
  		 if (this.input.keyboard.isDown(Phaser.Keyboard.LEFT) ) {
- 			//this.player.body.velocity.setTo(-40, 0);
- 			this.player.x-=2;
+ 			this.player.body.velocity.x = -300;
  		}
  		else if(this.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
- 		  	//this.player.body.velocity.setTo(40,0);
- 		  	this.player.x+=2;
- 		  	// implement additional speed variation in game
+				this.player.body.velocity.x = 300;
  		}
 
  		  
  		updateUI();
  }
+
 
 
 
