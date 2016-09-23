aff50a715349c2a69ea4352f548e8027c07f4b52
John Olav Haraldstad <john.haraldstad@hotmail.com>
2016-09-23 15:25:42 -0600

    Started adding hitboxes

--- js/main.js
+++ js/main.js
@@ -31,35 +31,47 @@ var balloon;
 var direction;
 var haveLeftPlayerHitbox;
 
+var playerCollisionGroup;
+var personCollisionGroup;
+var groundCollisionGroup;
+var balloonCollisionGroup;
+var towerCollisionGroup;
+
 RapidPrototyping.GameState.prototype.preload = function() {
   		console.log("Adding GameState. preload");
   		  //this.game.load.image('playerObject','Content/Images/personObject.png');
 
-  		  this.game.load.spritesheet('playerObject','Content/Images/playerSpritePNG.png.',320,64,4); 
-  		  // add person object sprite
-  		  this.game.load.spritesheet('personObject','Content/Images/personAnimation.png.',64,64,4); 
+  		  this.game.load.spritesheet('playerObject','Content/Images/playerSpritePNG.png.',320,64,4);
+
+  		  this.game.load.physics('player_physicsIdle', 'Content/Images/SpriteSheets/Firefighters/fireFighterIdle.json')
+  		  this.game.load.physics('player_physicsLeft', 'Content/Images/SpriteSheets/Firefighters/fireFighterLeft.json')
+  		  this.game.load.physics('player_physicsRight', 'Content/Images/SpriteSheets/Firefighters/fireFighterRight.json')
+// add person object sprite
+  		  this.game.load.spritesheet('personObject','Content/Images/business_peep.png.',64,64,1); 
+  		  this.game.load.spritesheet('Firefighters_AllSprites_150x64', 'Content/Images/SpriteSheets/Firefighters/Firefighters_AllSprites_150x64.png', 150, 64, 6);
+
   		  this.game.load.image('ground','Content/Images/ground.png');
   		//  this.game.load.atlas('person', 'Content/Images/personObject.png', 'Content/Images/fallingman.json'); 
   		  this.game.load.audio("backgrdSound","Content/Sound/bckgrdsound.ogg");
   		  this.game.load.image('balloon','Content/Images/Balloon.png');
-  		  this.game.load.image('tower', 'Content/Images/abandon_chinese_tower.png');
-  		  this.game.load.image('tower2', 'Content/Images/abandon_chinese_tower.png');
+  		  this.game.load.image('tower', 'Content/Images/Buildings/Apartment.png');
+  		  this.game.load.image('tower2', 'Content/Images/Buildings/Apartment.png');
   };
 
 RapidPrototyping.GameState.prototype.create = function() {
 //				game.add.sprite(0,0,'background');
 				console.log("Adding GameState. create");
 
-				this.GRAVITY = 500;
+				this.GRAVITY = 200;
 			  	this.game.stage.backgroundColor = "#4488AA";
 				this.game.physics.startSystem(Phaser.Physics.P2JS);
-				this.game.physics.p2.restitution = 1;
+				this.game.physics.p2.restitution = 0.8;
 
-				var playerCollisionGroup = game.physics.p2.createCollisionGroup();
-				var personCollisionGroup = game.physics.p2.createCollisionGroup();
-   				var groundCollisionGroup = game.physics.p2.createCollisionGroup();
-   				var balloonCollisionGroup = game.physics.p2.createCollisionGroup();
-   				var towerCollisionGroup = game.physics.p2.createCollisionGroup();
+				this.playerCollisionGroup = game.physics.p2.createCollisionGroup();
+				this.personCollisionGroup = game.physics.p2.createCollisionGroup();
+   				this.groundCollisionGroup = game.physics.p2.createCollisionGroup();
+   				this.balloonCollisionGroup = game.physics.p2.createCollisionGroup();
+   				this.towerCollisionGroup = game.physics.p2.createCollisionGroup();
 
  				this.game.physics.p2.updateBoundsCollisionGroup();
 
@@ -73,19 +85,21 @@ RapidPrototyping.GameState.prototype.create = function() {
           			groundBlock.body.kinematic = true;
 	          		groundBlock.body.immovable = true;
 	          		groundBlock.body.allowGravity=false;
-	          		groundBlock.body.setCollisionGroup(groundCollisionGroup)
-	          		groundBlock.body.collides(personCollisionGroup, loseLife, this);
+	          		groundBlock.body.setCollisionGroup(this.groundCollisionGroup)
+	          		groundBlock.body.collides(this.personCollisionGroup, loseLife, this);
           	   		this.ground.add(groundBlock);
       			}
 				//adding player object to screen
-				this.player = this.game.add.sprite(0,0,'playerObject',1);
+				this.player = this.game.add.sprite(0,0,'Firefighters_AllSprites_150x64',1);
 				this.player.scale.set(1.1);
 				this.player.anchor.setTo(0.5,0.5);
 				this.player.angle=0;
 				this.player.x= 800;
 				this.player.y = 800;
-				//this.player.animations.add('left',[1,2,3,4,5,6,7,8],8,true);				
-				//this.player.animations.play('left');
+				this.player.scale.set(1);
+				this.player.animations.add('idle',[0,1],2, true);
+				this.player.animations.add('left', [2,3], 2, true);
+				this.player.animations.add('right', [4,5], 2, true);	
 				this.game.input.keyboard.addKeyCapture([
           				Phaser.Keyboard.LEFT,
           				Phaser.Keyboard.RIGHT
@@ -98,9 +112,11 @@ RapidPrototyping.GameState.prototype.create = function() {
  				this.player.body.collideWorldBounds = true;
  				this.player.body.kinematic = true;
 				this.player.body.drag = 0.1;
-				this.player.body.setRectangle(320, 320, 0, 160);
-				this.player.body.setCollisionGroup(playerCollisionGroup);
-				this.player.body.collides(personCollisionGroup);
+				this.player.body.clearShapes();
+				this.player.body.loadPolygon('player_physicsIdle', 'Firefighters_AllSprites_150x64');
+				//this.player.body.setRectangle(200, 50, 0, 10);
+				this.player.body.setCollisionGroup(this.playerCollisionGroup);
+				this.player.body.collides([this.personCollisionGroup, this.towerCollisionGroup]);
 
       			this.tower = this.game.add.sprite(0,0,'tower',1);
       			this.tower.enableBody = true;
@@ -110,8 +126,8 @@ RapidPrototyping.GameState.prototype.create = function() {
 				this.tower.anchor.setTo(0.5,0.5);
 				this.tower.angle=0;
 				this.tower.body.kinematic = true;
-				this.tower.body.setCollisionGroup(towerCollisionGroup);
-				this.tower.body.collides([personCollisionGroup, playerCollisionGroup]);
+				this.tower.body.setCollisionGroup(this.towerCollisionGroup);
+				this.tower.body.collides([this.personCollisionGroup, this.playerCollisionGroup]);
 
  				this.tower2 = this.game.add.sprite(0,0,'tower2',1);
  				this.tower2.enableBody = true;
@@ -122,9 +138,10 @@ RapidPrototyping.GameState.prototype.create = function() {
 				this.tower2.angle=0;	
 				this.game.physics.p2.enable(this.tower2);
 				this.tower2.body.kinematic = true;
-				this.tower2.body.setCollisionGroup(towerCollisionGroup);
-				this.tower2.body.collides([personCollisionGroup, playerCollisionGroup]);	
+				this.tower2.body.setCollisionGroup(this.towerCollisionGroup);
+				this.tower2.body.collides([this.personCollisionGroup, this.playerCollisionGroup]);	
 
+				this.groupPerson = this.game.add.group();
 				// adding persons into scene
 				this.person= game.add.sprite(0,0,'personObject',1);
 				this.person.enableBody = true;
@@ -136,13 +153,15 @@ RapidPrototyping.GameState.prototype.create = function() {
 
 		      	this.person.body.collideWorldBounds = true;
                 this.person.body.velocity.x = 90;
-                this.person.body.velocity.y = -200;
-                this.person.body.setCollisionGroup(personCollisionGroup);
-                this.person.body.collides([playerCollisionGroup, towerCollisionGroup]);
-                this.person.body.collides(groundCollisionGroup, null, this);
-                this.person.body.collides(balloonCollisionGroup, hitBalloon, this);
-				//anim =this.person.animations.add('anim',[1,2,3,4,5,6,7,8],8,true);
+                this.person.body.velocity.y = -100;
+                this.person.body.setCollisionGroup(this.personCollisionGroup);
+                this.person.body.collides([this.playerCollisionGroup, this.towerCollisionGroup]);
+                this.person.body.collides(this.groundCollisionGroup, null, this);
+                this.person.body.collides(this.balloonCollisionGroup, hitBalloon, this);
+				//this.person.animations.add('anim',[0,1,2,3],4,true);
+				//this.person.animations.play('anim');
 				//anim.enableUpdate=true;
+				this.groupPerson.add(this.person);
 				personAlive = true;
 				rotatingSpeed = 10;
 
@@ -174,8 +193,8 @@ RapidPrototyping.GameState.prototype.create = function() {
 	                	}
 
 						this.game.physics.enable(this.balloon,Phaser.Physics.P2JS);
-						this.balloon.body.setCollisionGroup(balloonCollisionGroup);
-						this.balloon.body.collides(personCollisionGroup, null, this);
+						this.balloon.body.setCollisionGroup(this.balloonCollisionGroup);
+						this.balloon.body.collides(this.personCollisionGroup, null, this);
 	                	if (j == 0)
 	                	{
 	                		this.balloon.body.velocity.x =100;
@@ -209,7 +228,10 @@ RapidPrototyping.GameState.prototype.create = function() {
 				music.play();
 
 				game.physics.p2.setImpactEvents(true);
- 	console.log(this.player.body.debug);
+
+				//Activate for more people!
+				//.game.time.events.add(Phaser.Timer.SECOND * 10, makeNewPerson, this);
+ 	//console.log(this.person.body.debug);
 };
 
 function loseLife(ground, person)
@@ -220,10 +242,30 @@ function loseLife(ground, person)
 	person.sprite.body.enable = true;
 	person.sprite.body.velocity.x = 90;
 	person.sprite.body.velocity.y = -200;
+	livesLeft = livesLeft-1;
+	if(livesLeft==0)
+		playerDead();
+}
 
-	//livesLeft = livesLeft-1;
-	//if(livesLeft==0)
-		//playerDead();
+function makeNewPerson()
+{
+	this.person= game.add.sprite(0,0,'personObject',1);
+	this.person.enableBody = true;
+	this.person.x= 1000;
+	this.person.y = 500;
+	this.person.scale.set(1);
+	this.person.anchor.setTo(0.5,0.5);
+	this.game.physics.enable(this.person,Phaser.Physics.P2JS);
+
+  	this.person.body.collideWorldBounds = true;
+    this.person.body.velocity.x = 90;
+    this.person.body.velocity.y = -100;
+    this.person.body.setCollisionGroup(this.personCollisionGroup);
+    this.person.body.collides([this.playerCollisionGroup, this.towerCollisionGroup]);
+    this.person.body.collides(this.groundCollisionGroup, null, this);
+    this.person.body.collides(this.balloonCollisionGroup, hitBalloon, this);
+
+	this.groupPerson.add(this.person);
 }
 
 function playerDead()
@@ -254,31 +296,47 @@ function findAngle(a, b)
 
  RapidPrototyping.GameState.prototype.update = function() {
 
- 	game.debug.body(this.player); 	game.debug.body(this.person);
+ 	//game.debug.body(this.player); 	game.debug.body(this.person);
 
 
 	this.player.body.velocity.x = 0;
 	 if (this.input.keyboard.isDown(Phaser.Keyboard.LEFT) && !Phaser.Rectangle.intersects(this.player, this.tower)) {
 		
 		this.player.body.moveLeft(1000);
-		this.player.play('left');
-
-			this.player.body.angle= -45;
-
+		this.player.animations.play('left', 10, true);
+		this.player.body.angle = -45;
+		//this.player.body.clearShapes();
+		//this.player.body.loadPolygon('player_physicsLeft', 'Firefighters_AllSprites_150x64');
+		//this.player.body.setRectangle(150, 10, 0, 10, -0);
+		//this.player.body.setCollisionGroup(this.playerCollisionGroup);
+		//console.log(this.player.body.debug);
 		this.direction = -1;
 	}
 	else if(this.input.keyboard.isDown(Phaser.Keyboard.RIGHT) && !Phaser.Rectangle.intersects(this.player, this.tower2)){
 		
 		this.player.body.moveRight(1000);
-		this.player.play('left');
-
-		this.player.body.angle= 45;
+		this.player.animations.play('right', 10, true);
+		this.player.body.angle = 45;
+		//this.player.body.clearShapes();
+		//this.player.body.loadPolygon('player_physicsRight', 'Firefighters_AllSprites_150x64');
+		//this.player.body.sprite.rotation = 10;
+		//this.player.body.setRectangle(150, 10, 0, 10, 0);
+		//this.player.body.setCollisionGroup(this.playerCollisionGroup);
+		//console.log(this.player.body.debug);
+		//this.player.body.angle= 45;
 		this.direction = 1;
 	}
 	else
 	{
 		this.direction = 0;
 		this.player.body.angle = 0;
+		if (this.player.animations.currentAnim != 'idle')
+		{			
+			this.player.animations.play('idle');
+			//this.player.body.setRectangle(150, 10, 0, 10, 0);
+			this.player.body.setCollisionGroup(this.playerCollisionGroup);
+		}
+
 	}
 
 	for (var i = 0; i < this.group1.length; i++)
@@ -289,15 +347,6 @@ function findAngle(a, b)
 			this.group1.children[i].body.x = -20;
 			this.group1.children[i].body.enable = true;
 		}
-		/*
-		if (this.game.physics.p2.collide(this.person, this.group1.children[i]))
-		{
-			var vector = findAngle(this.person.body.position, this.group1.children[i].position);
-			this.person.body.velocity.x *= vector[0];
-			this.group1.remove(this.group1.children[i]);
-			rotating = true;
-			return;
-		}*/
 	}
 
 	for (var i = 0; i < this.group2.length; i++)
@@ -308,15 +357,6 @@ function findAngle(a, b)
 			this.group2.children[i].body.x = game.width+25;
 			this.group2.children[i].body.enable = true;
 		}
-		/*
-		if (this.game.physics.p2.collide(this.person, this.group2.children[i]))
-		{
-			var vector = findAngle(this.person.body.position, this.group2.children[i].position);
-			this.person.body.velocity.x *= vector[0];
-			this.group2.remove(this.group2.children[i]);
-			rotating = true;
-			return;
-		}*/
 	}
 
 	for (var i = 0; i < this.group3.length; i++)
@@ -327,15 +367,6 @@ function findAngle(a, b)
 			this.group3.children[i].body.x = -20;
 			this.group3.children[i].body.enable = true;
 		}
-		/*
-		if (this.game.physics.p2.collide(this.person, this.group3.children[i]))
-		{
-			var vector = findAngle(this.person.body.position, this.group3.children[i].position);
-			this.person.body.velocity.x *= vector[0];
-			this.group3.remove(this.group3.children[i]);
-			rotating = true;
-			return;
-		}*/
 	}
 
 	if (this.person.body.velocity.x > 1500)
